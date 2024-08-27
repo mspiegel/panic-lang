50e876bb064de58b117933323841814d6a9902ad
Michael Spiegel <michael.m.spiegel@gmail.com>
2024-08-26 21:48:47 -0400

    use clap library

--- src/interpreter/main.rs
+++ src/interpreter/main.rs
@@ -1,29 +1,53 @@
 use std::io;
 use std::io::BufRead;
 
-use pest::Parser;
 //use pest_ascii_tree::print_ascii_tree;
+use clap::Subcommand;
 
 use panic_lang::parser::peg_grammar::PanicParser;
 use panic_lang::parser::peg_grammar::Rule;
 use panic_lang::parser::syntax_tree::program;
 use panic_lang::parser::syntax_tree::Program;
 
+#[derive(Debug, clap::Parser)]
+#[clap(name = "pani", version)]
+pub struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// parse the input file, and pretty-print to standard output
+    Print,
+    /// parse the input file, apply reductions, and pretty-print to standard output
+    Reduce,
+    /// parse the input file, and run the program
+    Run,
+}
+
 fn command_print(_input: &str, prog: Program) {
     print!("{}", prog);
 }
 
 fn main() {
+    let cli = <Cli as clap::Parser>::parse();
+
     let stdin = io::stdin();
     let lines: Vec<_> = stdin.lock().lines().collect::<Result<_, _>>().unwrap();
     let input = lines.join("\n");
 
-    let pairs = PanicParser::parse(Rule::program, &input);
+    let pairs = <PanicParser as pest::Parser<_>>::parse(Rule::program, &input);
     if pairs.is_err() {
         panic!("{}", pairs.unwrap_err());
     }
     let top_node = pairs.unwrap().next().unwrap();
     //    print_ascii_tree(top_node.clone());
     let prog = program(top_node);
-    command_print(&input, prog);
+
+    match cli.command {
+        Command::Print => command_print(&input, prog),
+        Command::Reduce => todo!(),
+        Command::Run => todo!(),
+    }
 }
